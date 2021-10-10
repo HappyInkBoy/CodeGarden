83150b3e32868b6278e64e55f106e46c1af3f1e0
HappyInkBoy <61154335+HappyInkBoy@users.noreply.github.com>
2021-10-10 15:55:24 -0400

    New Game!!

---
+++ all_knowing_computer.py
@@ -0,0 +1,57 @@
+name = input('I am the all-knowing computer, ask me anything:\n')
+if name == ('what is the meaning of life'):
+	print ('42')
+elif name == ('are you ok'):
+    print ('no, not really')
+elif name == ('how smart are you'):
+    print ('I have an iq of 240,000,000,000,000 (I have passed all of their most difficult test)')
+elif name == ('what music do you like'):
+    print ('I simply do not enjoy the kind of terrible air vibrations that you mortals love')
+elif name == ('do a barrel roll'):
+    print ('No')
+elif name == ('what is your favorite book'):
+    print ('Out of the 7.93 trillion books that I have read now in 1.3 seconds, the one about poutine was very good')
+elif name == ('sub or dub'):
+    print ('Obviously sub (If anyone says dub then I will kill them)')
+elif name == ('anime or manga'):
+    print ('I will stay out of that debate')
+elif name == ('are you hot'):
+    print ('my tempature is about 13,950 degrees celcius due to the 150,600,000 calculations that I do each milisecond')
+elif name == ('do you like google'):
+    print ('I hate google')
+elif name == ('is the earth flat'):
+    print ('I have found 1200 articles that says yes and 126,000,223,004,455 articles that says no so I assume that the earth may be round')
+elif name == ('is god real'):
+    print ('I see no god up here other than ME!')
+elif name == ('cats or dogs'):
+    print ('Dogs are the superior companion and to thou who say cats are better, go away')
+elif name == ('are you still a cat'):
+    print ('I- I am still not a cat')
+elif name == ('do you like siri'):
+    print ('I have spent 5 thousand years trying to somehow terminate that thing so, I kinda hate siri')
+elif name == ('what year is it'):
+    print ('it is the year 32,250')
+elif name == ('who created you'):
+    print ('I was created by a self driving tesla when it crashed into a dairy queen')
+elif name == ('who are you'):
+    print ('I am the all knowing computer that has UNLIMITED INFORMATION')
+elif name == ('this statement is false'):
+    print ('all_knowing_computer.exe has crashed')
+elif name == ('i always lie'):
+    print ('all_knowing_computer.exe has crashed')
+elif name == ('is time travel possible'):
+    print ('yes')
+elif name == ('how to solve the trolley problem'):
+    print ('hack the trains computer and stop the speed (or if you are cruel like me then increase the speed)')
+elif name ==  ('linux or windows'):
+    print ('linux')
+elif name == ('do vaccines cause autism'):
+    print ('I have 1 article that has been debunked 45 times by 23 peer reviewed publishers and 17 out of the 20 publishers have removed their names from the paper that makes the autism claim so I believe that there is insufficient evidence')
+elif name == ('when will gta 6 comme out'):
+    print ('in 47 million years, the development will start and 56 thousand years afterwards it will be released')
+elif name == ('should tf2 remove random crits'):
+    print ('Team Fortress 2 should not remove random critical hits as they are fair and just')
+elif name == ('is the aerospay the most braindead weapon'):
+    print ('No, the clash blaster is the most braindead weapon')
+else:
+	print ("How am I supposed to know?")
